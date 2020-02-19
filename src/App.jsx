d90dc25302cbf12de02e3f7bfbcb0e6594615a2f
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2020-02-19 18:57:42 -0300

    Exercício do Fusca

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import './App.css';
-import Header from './layout/Header';
-import Button from './layout/Button';
-import Main from './layout/Main';
-import Teste from './containers/Teste';
+import React, { Component } from 'react'
+import Fusca from './layout/Fusca';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header nome="cassiel fica quieto" />
-      <Button cor="white" fundo="black" >
-         Magnanimo
-      </Button>
-      <Teste />
-      <Main />
-    </div>
-  );
+export class App extends Component {
+  state = {
+    fuscas: [
+      {img:'https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2019/09/vw-fusca-eletrico-5.jpg', descricao: ' fusca em otimo estado '},
+      {img:'https://i.ytimg.com/vi/bbRRXF4YQpM/hqdefault.jpg', descricao: ' fusca em otimo estado '}
+    ]
+  }
+
+  deletar=(i)=>{
+     const fuscas=this.state.fuscas;
+     console.log(i);
+     fuscas.splice(i,1);
+     this.setState({fuscas})
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.fuscas.map((fusca,i)=>(
+          <Fusca img={fusca.img} descricao={fusca.descricao} remove={() => this.deletar(i)}  />
+        )) }
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/layout/Fusca.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+const Fusca = (props) => {
+  return (
+    <div className="container">
+      <Card>
+        <CardImg top width="100%" src={props.img} alt="Card image cap" />
+        <CardBody>
+                   <Button onClick={()=>alert(props.descricao)} >Mostrar</Button>
+                   <Button onClick={props.remove}>Deletar</Button>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default Fusca;
