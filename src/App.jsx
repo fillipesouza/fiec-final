4d6537e5591c98e994d7e11d475e8adbe416e16b
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2020-02-19 21:23:39 -0300

    Adicionando o axios e fazendo http

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react'
 import Fusca from './layout/Fusca';
+import axios from 'axios';
 
 export class App extends Component {
   state = {
-    fuscas: [
-      {img:'https://cdn-motorshow-ssl.akamaized.net/wp-content/uploads/sites/2/2019/09/vw-fusca-eletrico-5.jpg', descricao: ' fusca em otimo estado '},
-      {img:'https://i.ytimg.com/vi/bbRRXF4YQpM/hqdefault.jpg', descricao: ' fusca em otimo estado '}
-    ]
+    fuscas : []
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('http://localhost:38000/fuscas');    
+    this.setState({ fuscas: res.data });    
   }
 
   deletar=(i)=>{
+     
      const fuscas=this.state.fuscas;
      console.log(i);
      fuscas.splice(i,1);
+     //console.log(fuscas);
      this.setState({fuscas})
   }
 
@@ -20,7 +25,7 @@ export class App extends Component {
     return (
       <div>
         {this.state.fuscas.map((fusca,i)=>(
-          <Fusca img={fusca.img} descricao={fusca.descricao} remove={() => this.deletar(i)}  />
+          <Fusca key={i} img={fusca.img} descricao={fusca.descricao} remove={() => this.deletar(i)}  />
         )) }
         
       </div>
